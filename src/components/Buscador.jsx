91c30cb50b155db3d003e24e17c3345b0864be5e
anaais andrea <anaisandreatp.19@gmail.com>
2023-10-25 01:26:49 -0300

    Finish App

---
+++ src/components/Buscador.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react'
+
+const Buscador = ({ searchValue }) => {
+    const [value, setValue] = useState('')
+
+    const handleChange = (event) => {
+        const newValue = event.target.value;
+        setValue(newValue);
+
+        // Llama a la función handleSearch con el nuevo valor
+        searchValue(newValue);
+    };
+
+    return (
+        <div className='mb-5'>
+            <input
+                type="text"
+                className="form-control w-75 mx-auto"
+                aria-label="Small"
+                placeholder='Busca Aquí ...'
+                aria-describedby="inputGroup-sizing-sm"
+                value={value} // Vincula el valor del input al estado local
+                onChange={handleChange} // Agrega el evento onChange
+            />
+        </div>
+    )
+}
+
+export default Buscador
---
+++ src/components/MiApi.jsx
@@ -0,0 +1,76 @@
+import React, { useEffect, useState } from 'react';
+
+const MiApi = ({ searchValue }) => {
+    const [data, setData] = useState({});
+    const [indicadores, setIndicadores] = useState([]);
+    const [sortDirection, setSortDirection] = useState('asc'); // Estado para la dirección del orden
+
+    useEffect(() => {
+        // Función asincrónica para realizar la solicitud
+        async function fetchData() {
+            try {
+                const apiUrl = `https://mindicador.cl/api`;
+                const response = await fetch(apiUrl);
+                if (!response.ok) {
+                    throw new Error('No se pudo obtener datos de la API');
+                }
+                const data = await response.json();
+                setData(data);
+                // Agregar las propiedades del objeto a un arreglo
+                const indicadoresArray = Object.keys(data).map(indicador => data[indicador]);
+                // Excluir las primeras 3 tarjetas aquí al cargar los datos
+                setIndicadores(indicadoresArray.slice(3));
+            } catch (error) {
+                console.error('Error al obtener datos de la API:', error);
+            }
+        }
+
+        // Llama a la función asincrónica
+        fetchData();
+    }, []);
+
+    // Filtra los indicadores según el término de búsqueda (searchValue)
+    const indicadoresFiltrados = indicadores.filter(indicador =>
+        indicador.nombre.toLowerCase().includes(searchValue.toLowerCase())
+    );
+
+    // Función para cambiar la dirección del orden y ordenar los indicadores
+    const handleSort = (direction) => {
+        // Cambia la dirección de orden
+        setSortDirection(direction);
+
+        // Ordena los indicadores según la dirección actual del orden
+        const sortedIndicadores = [...indicadoresFiltrados].sort((a, b) => {
+            if (direction === 'asc') {
+                return a.nombre.localeCompare(b.nombre);
+            } else {
+                return b.nombre.localeCompare(a.nombre);
+            }
+        });
+
+        setIndicadores(sortedIndicadores);
+    };
+
+    return (
+        <div>
+            <div className="button-container my-3">
+                <button className='mx-1' onClick={() => handleSort('asc')}>Ascendente</button>
+                <button className='mx-1' onClick={() => handleSort('desc')}>Descendente</button>
+            </div>
+            <div className="card-container">
+                {indicadoresFiltrados.map(indicador => (
+                    <div className="card mb-3" key={indicador.codigo}>
+                        <div className="card-body">
+                            <h3 className="card-title">{indicador.nombre}</h3>
+                            <p className="card-text">Valor: {indicador.valor}</p>
+                            <p className="card-text">Fecha: {indicador.fecha}</p>
+                            <p className="card-text">Unidad de medida: {indicador.unidad_medida}</p>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+}
+
+export default MiApi;
